6ffed8af819fd94a271e0cd845c3a2a0c595fea2
sunsetblvdtunehaumea <118460805+sunsetblvdtunehaumea@users.noreply.github.com>
2022-11-17 10:41:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,12 @@ import streamlit
 streamlit.title('My parents new Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega and Oats')
-streamlit.text('Kale')
-streamlit.text('Hard Boiled Eggs')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
