01bf33b9610933e0b994b1122b82fe58968a211f
sunsetblvdtunehaumea <118460805+sunsetblvdtunehaumea@users.noreply.github.com>
2022-11-17 10:39:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit
 
 streamlit.title('My parents new Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega and Oats')
+streamlit.text('Kale')
+streamlit.text('Hard Boiled Eggs')
+
+
+
+
